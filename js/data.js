6e5de69198362a6061887dfa30e6f024a833fcd5
mark0613 <61940915+mark0613@users.noreply.github.com>
2021-10-07 20:38:50 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,6 +2,3 @@ const Data = {
     "the key word of the website url" : ["your username or account", "your password"],
     "another key word" : ["account", "password"]
 }
-const Account = ["username", "user", "account", "name", "email", "nid", "login"];
-const Password = ["password", "pswd", "passwd", "pass", "pwd"];
-
